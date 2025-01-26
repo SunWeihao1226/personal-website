63e3ea36404f9f7e12e0dd979a6e877afaf4f691
Weihao Sun <sunweihao1226@gmail.com>
2025-01-25 20:03:15 -0500

    change mobile view

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -85,7 +85,8 @@ const MobileNav: FC<{navSections: SectionId[]; currentSection: SectionId | null}
               leave="transition-opacity ease-linear duration-300"
               leaveFrom="opacity-100"
               leaveTo="opacity-0">
-              <Dialog.Overlay className="fixed inset-0 bg-stone-900 bg-opacity-75" />
+              /* <Dialog.Overlay className="fixed inset-0 bg-stone-900 bg-opacity-75" /> */
+              <Dialog.Overlay className="fixed inset-0 bg-transparent" />
             </Transition.Child>
             <Transition.Child
               as={Fragment}
